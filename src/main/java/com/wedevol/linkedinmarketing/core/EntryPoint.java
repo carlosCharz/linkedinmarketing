fe121d3dd876d7f0be5039e0be55ac2b69cad670
Carlos Becerra <carlos.becerra1234@gmail.com>
2020-04-23 17:36:06 -0500

    remove unused log

--- src/main/java/com/wedevol/linkedinmarketing/core/EntryPoint.java
+++ src/main/java/com/wedevol/linkedinmarketing/core/EntryPoint.java
@@ -1,7 +1,5 @@
 package com.wedevol.linkedinmarketing.core;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -14,8 +12,6 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
 public class EntryPoint extends SpringBootServletInitializer {
-
-  private static final Logger logger = LoggerFactory.getLogger(EntryPoint.class);
 
   @Override
   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {

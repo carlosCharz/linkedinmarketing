6a86e119966fd79e901219581428286789627415
Carlos Becerra <carlos.becerra1234@gmail.com>
2020-05-13 18:14:18 -0500

    Convert tests to junit5

--- src/test/java/com/wedevol/linkedinmarketing/core/service/impl/DataProcessorServiceImplTest.java
+++ src/test/java/com/wedevol/linkedinmarketing/core/service/impl/DataProcessorServiceImplTest.java
@@ -1,25 +1,28 @@
 package com.wedevol.linkedinmarketing.core.service.impl;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.util.Assert;
 
-@RunWith(MockitoJUnitRunner.class)
 public class DataProcessorServiceImplTest {
 
     @Test
-    public void lineArraySplitTest() {
+    @DisplayName("Line array split not empty")
+    public void lineArraySplitNotEmptyTest() {
         String line = "4567|arturo|perez|teleport engineering manager|Germany|Telecommunications|3|176";
         String[] lineParts = line.split("\\|");
         Assert.notEmpty(lineParts, "The array is empty");
     }
 
-    @Test(expected = Exception.class)
+    @Test
+    @DisplayName("Line array split with exception")
     public void lineArraySplitWithExceptionTest() {
-        String line = "4567|arturo|perez|||||";
-        String[] lineParts = line.split("\\|");
-        lineParts[5].trim();
+        Assertions.assertThrows(Exception.class, () -> {
+            String line = "4567|arturo|perez|||||";
+            String[] lineParts = line.split("\\|");
+            lineParts[5].trim();
+        });
     }
 
     // TODO add more tests
